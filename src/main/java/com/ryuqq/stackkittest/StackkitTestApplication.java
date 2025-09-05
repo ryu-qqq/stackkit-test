57de944c9092eae09a3d8854d944449a9611c936
RyuSangwon <rsw2@connectly.co.kr>
2025-09-05 12:54:29 +0900

    stackkit test module

---
+++ src/main/java/com/ryuqq/stackkittest/StackkitTestApplication.java
@@ -0,0 +1,13 @@
+package com.ryuqq.stackkittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackkitTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StackkitTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ryuqq/stackkittest/StackkitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryuqq.stackkittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackkitTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
